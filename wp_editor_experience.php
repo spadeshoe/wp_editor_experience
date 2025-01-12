e78102da30eeac72f06585bd2485626c3afd32e7
Jason Shoemaker <ops@spadefootmedia.com>
2025-01-12 15:35:15 -0500

    adds essential file

---
+++ wp_editor_experience.php
@@ -0,0 +1,19 @@
+<?php
+/**
+	* Plugin Name: WP Editor Experience
+	* Description: Experiments in curating the editor experience in the Admin dashboard
+	* Version: 0.1.0
+	* Author: Spadefoot Media, LLC
+	* License: GPL-3.0
+	* Text Domain: wp-editor-experience
+
+*/
+
+if( ! defined('ABSPATH') ){
+	exit;
+}
+
+define('WP_EDITOR_UX__VERSION', '0.1.0');
+define('WP_EDITOR_UX__DIRECTORY_PATH', plugin_dir_path( __FILE__ ));
+define('WP_EDITOR_UX__DIRECTORY_URL', plugin_dir_url( __FILE__ ));
+
