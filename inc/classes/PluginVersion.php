25c8cc689283f1e68879a78ade48536f7ab88b81
Jason Shoemaker <ops@spadefootmedia.com>
2025-01-20 15:47:30 -0500

    builds out ManagePluginVersion class and roughs out init tasks

--- inc/classes/PluginVersion.php
+++ inc/classes/PluginVersion.php
@@ -23,21 +23,41 @@ class ManagePluginVersion {
     /**
      * Set the plugin version to the options table
      */
-    public function set_plugin_version(){
+    public function set_plugin_version(): void{
+
+        if(current_user_can('manage_options')){
+
+            update_option($this->plugin_version_option_slug, $this->plugin_version);
+
+        }
 
     }
 
     /**
      * Get the plugin version from the options table
+     * 
+     * @return string $plugin_option - string from options table or empty string if no option value returned
+     * 
      */
-    public function get_plugin_version(){
+    public function get_plugin_version(): string{
 
+        $plugin_option = get_option($this->plugin_version_option_slug, '');
+
+        return $plugin_option;
     }
 
     /**
      * Check if the plugin version matches that set in the options table
+     * 
+     * @return bool true/false - compares option value set via WP_EDITOR_UX__VERSION constant with value set in the options table. Returns true if values match, false otherwise.
      */
-    public function is_new_version(){
+    public function is_new_version(): bool{
+
+        if($this->get_plugin_version() == $this->plugin_version){
+            return true;
+        }
+
+        return false;
 
     }
 
--- wp_editor_experience.php
+++ wp_editor_experience.php
@@ -13,7 +13,9 @@ if( ! defined('ABSPATH') ){
 	exit;
 }
 
-define('WP_EDITOR_UX__VERSION', '0.1.0');
+define('WP_EDITOR_UX__VERSION', '0.1.0-new-version');
 define('WP_EDITOR_UX__DIRECTORY_PATH', plugin_dir_path( __FILE__ ));
 define('WP_EDITOR_UX__DIRECTORY_URL', plugin_dir_url( __FILE__ ));
 
+require WP_EDITOR_UX__DIRECTORY_PATH . '/inc/classes/PluginVersion.php';
+require WP_EDITOR_UX__DIRECTORY_PATH . '/inc/init.php';
